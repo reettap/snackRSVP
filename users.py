61e8e2b75d5ccb39848c1cb57c825bc5e7c56a36
Reetta Puska <22945591+reettap@users.noreply.github.com>
2026-02-19 22:51:33 +0200

    change password generation method

--- users.py
+++ users.py
@@ -7,7 +7,7 @@ def get_user(user_id):
     return result[0] if result else None
 
 def create_user(username, password):
-    password_hash = generate_password_hash(password, method='pbkdf2')
+    password_hash = generate_password_hash(password)
     sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
     db.execute(sql, [username, password_hash])
 
